a367094113d9be6732e59911f24ac221faaa0340
Christian <christian@factual.com>
2014-06-19 18:17:09 -0700

    Updated style & main

--- js/main.js
+++ js/main.js
@@ -2,13 +2,20 @@
 
 var main = ['Name', 'Telephone', 'Address', 'Locality', 'Region', 'Geocode'];
 var other = ['Post Code', 'Country', 'Website', 'Category ID'];
-
-var json = $.getJSON("./resources/peets_input_report.json", function(json) {
+var commitID = "H4sIAAAAAAAAAA3HyxECQQgA0Yim-AwwEIIBGABgjV50D-7B8N0-vKoGdIPzd3u-7xDo9JA9R7fzEKw5siKHLHZvUzZEIHBE6ZYy5nBfnFWJ6lHlvnk1fI7xOr4n4BUBCbJRsMklrGm0qVQ1S7OtQ68Lw8UihvMP-2NSlJAAAAA";
+//Get the JSON file from the server
+var commitReport;
+$.getJSON("http://localhost:8888/resources/peets_input_report.json", function(json) {
     console.log(json); // this will show the info it in firebug console
+    commitReport = json;
 });
 
 function MenuBarCtrl($scope){
 	$scope.mainAttribs = main;
 	$scope.otherAttribs = other;
 
+}
+
+function TableCtrl($scope){
+	// $scope.rawInput = commitReport.input.payloadRaw;
 }
